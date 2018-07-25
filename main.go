038c2b38d7a8fd844815d340bc6b35b2e65032a3
Ricky Pai <rickyp999+github@gmail.com>
2018-07-24 17:03:25 -0700

    refactor into smaller methods

--- main.go
+++ main.go
@@ -11,6 +11,14 @@ import (
 	"golang.org/x/oauth2"
 )
 
+const godel1Dir = "/Users/ricky/workspace/src/github.com/vsco/godel"
+const godel2Dir = "/Users/ricky/workspace/src/github.com/vsco/godel2"
+
+var godelDirs = []string{
+	godel1Dir,
+	godel2Dir,
+}
+
 func main() {
 	githubToken := os.Getenv("GITHUB_TOKEN")
 	if githubToken == "" {
@@ -39,62 +47,64 @@ func main() {
 		}
 
 		for _, pr := range prs {
-			if pr.GetUser().GetLogin() != "rickypai" {
-				continue
-			}
-
-			// delete closed branches
-			if pr.GetState() == "closed" {
-				cmd := exec.Command("git", "show", pr.Head.GetRef())
-				cmd.Dir = godelDir
-				_, err := cmd.Output()
-				if err != nil {
-					continue
-				}
-
-				log.Printf("deleting '%v'", pr.Head.GetRef())
-
-				execCommandDirs(
-					[]string{
-						godelDir,
-						godel2Dir,
-					},
-					"git", "branch", "-D", pr.Head.GetRef(),
-				)
-			}
-
-			if pr.GetState() == "open" {
-				cmd := exec.Command("git", "show", pr.Head.GetRef())
-				cmd.Dir = godelDir
-				_, err := cmd.Output()
-				if err != nil {
-					continue
-				}
-
-				log.Printf("setting PR for '%v'", pr.Head.GetRef())
-
-				execCommandDirs(
-					[]string{
-						godelDir,
-						godel2Dir,
-					},
-					"twig", "--branch", pr.Head.GetRef(), "issue", strconv.Itoa(pr.GetNumber()),
-				)
-
-				execCommandDirs(
-					[]string{
-						godelDir,
-						godel2Dir,
-					},
-					"twig", "--branch", pr.Head.GetRef(), "diff-branch", pr.Base.GetRef(),
-				)
+			for _, godelDir := range godelDirs {
+				syncPR(pr, godelDir)
 			}
 		}
 	}
 }
 
-const godelDir = "/Users/ricky/workspace/src/github.com/vsco/godel"
-const godel2Dir = "/Users/ricky/workspace/src/github.com/vsco/godel2"
+func syncPR(pr *github.PullRequest, localDir string) {
+	if pr.GetUser().GetLogin() != "rickypai" {
+		return
+	}
+
+	if err := branchExists(pr.Head.GetRef(), localDir); err != nil {
+		return
+	}
+
+	if pr.GetState() == "closed" {
+		syncClosedPR(pr, localDir)
+	} else if pr.GetState() == "open" {
+		syncOpenPR(pr, localDir)
+	}
+}
+
+func branchExists(branchName, localDir string) error {
+	cmd := exec.Command("git", "show", branchName)
+	cmd.Dir = localDir
+	_, err := cmd.Output()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func syncClosedPR(pr *github.PullRequest, localDir string) {
+	log.Printf("deleting '%v'", pr.Head.GetRef())
+
+	// delete closed branches
+	execCommand(
+		localDir,
+		"git", "branch", "-D", pr.Head.GetRef(),
+	)
+}
+
+func syncOpenPR(pr *github.PullRequest, localDir string) {
+	log.Printf("setting PR for '%v'", pr.Head.GetRef())
+
+	// set GH issue
+	execCommand(
+		localDir,
+		"twig", "--branch", pr.Head.GetRef(), "issue", strconv.Itoa(pr.GetNumber()),
+	)
+
+	// set diff branch
+	execCommand(
+		localDir,
+		"twig", "--branch", pr.Head.GetRef(), "diff-branch", pr.Base.GetRef(),
+	)
+}
 
 func execCommandDirs(dirs []string, name string, arg ...string) {
 	for _, dir := range dirs {
